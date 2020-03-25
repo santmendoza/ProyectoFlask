e2b0c9f479b9cbe7bab4d417db85b323dbb3e490
unknown <santiagomendoza1997@gmail.com>
2020-03-24 22:15:57 -0500

    Probando

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,request
+import re
 #objeto
 app=Flask(__name__)
 
@@ -10,7 +10,23 @@ def home():
     return render_template('home.html')
 
 
+#expresion que operamos en la ventana
+patron=re.compile('^([A-Z]{1}[0-9]{3}[a-z]+[\W]{3})$')
+
+@app.route('/add_contact',methods=['POST'])
+def add_contact():
+    if request.method=='POST':
+        expresion=request.form['expresion']
+
+        if(patron.match(expresion)):
+
+                return "¡Es correcta la expresión!"
+        else:
+                return "¡Es incorrecta la expresión!"
+    
 #escuchar siempre
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
+
+
 
